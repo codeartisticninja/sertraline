2ea056f872a52e1886e779102d14c6907253fedd
codeartistic.ninja <the@codeartistic.ninja>
2017-08-31 19:53:04 +0200

    proper ship movement

--- src/script/_classes/scenes/actors/Ship.ts
+++ src/script/_classes/scenes/actors/Ship.ts
@@ -13,13 +13,26 @@ class Ship extends Actor {
     super(scene, obj);
     this.addAnimation("be",  [ 0, 1, 2, 3, 4, 5, 6, 7]);
     this.playAnimation("be");
+    this.momentum = .99;
   }
 
   update() {
     var joy = this.scene.game.joypad;
-    this.velocity.copyFrom(joy.dir).multiplyXY(8);
+    var width = this.scene.game.canvas.width;
+    var height = this.scene.game.canvas.height;
+    this.rotation += joy.dir.x * .25;
+    this.velocity.addXY(-Math.sin(this.rotation)*joy.dir.y, Math.cos(this.rotation)*joy.dir.y);
     super.update();
-    this.scene.camera.copyFrom(this.position).subtractXY(this.scene.game.canvas.width/2, this.scene.game.canvas.height/2);
+    if (this.position.x < 0) this.position.x += width;
+    if (this.position.y < 0) this.position.y += height;
+    if (this.position.x > width) this.position.x -= width;
+    if (this.position.y > height) this.position.y -= height;
+    this.scene.camera.copyFrom(this.position).subtractXY(width/2, height/2);
+  }
+
+  render() {
+    this.scene.game.ctx.strokeStyle = "cyan";
+    super.render();
   }
 
 
